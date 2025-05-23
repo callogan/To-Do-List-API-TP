ce3e5e73a11a2ffd4a02c467e7172ae967d25a35
Ruslan Kazmiryk <callogan217@gmail.com>
2025-05-23 11:30:09 +0300

    Create models, incorporate them via admin.py, make necessary amendments and run migrations

--- to_do_list_api/admin.py
+++ to_do_list_api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from to_do_list_api.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "due_date")
+    list_filter = ("status", "due_date")
+    search_fields = ("title", "description")
+    readonly_fields = ("id",)
+    fieldsets = (
+        ("Main information", {
+            "fields": ("id", "title", "description")
+        }),
+        ("Status and time", {
+            "fields": ("status", "due_date")
+        }),
+    )
+
+
+admin.site.register(Task, TaskAdmin)
+
--- to_do_list_api/models.py
+++ to_do_list_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = [("P", "Pending"), ("IP", "In-progress"), ("C", "Completed")]
+
+    title = models.TextField()
+    description = models.TextField(blank=True)
+    due_date = models.DateTimeField(null=True)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default="P")
+
+    class Meta:
+        ordering = ["status"]
