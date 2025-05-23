42313fa9faccf73750337f08eaa9cd897bbb3c1c
Ruslan Kazmiryk <callogan217@gmail.com>
2025-05-23 12:19:05 +0300

    Add throttling

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    )
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "300/day"},
 }
 
 SIMPLE_JWT = {
