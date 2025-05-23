2feda81ffbd4b4bd73951bdbcf6e48d0538877d9
Ruslan Kazmiryk <callogan217@gmail.com>
2025-05-23 11:47:26 +0300

    Create views

--- to_do_list_api/views.py
+++ to_do_list_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from to_do_list_api.models import Task
+from to_do_list_api.serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["status", "due_date"]
+
+    def list(self, request, *args, **kwargs):
+       return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
